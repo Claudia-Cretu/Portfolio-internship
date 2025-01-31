145f9ecd3a462531b9ca17b23cd6405c75e1221f
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-01-31 11:43:59 +0100

    create pages, routes, servers

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectPageComponent },
+];
