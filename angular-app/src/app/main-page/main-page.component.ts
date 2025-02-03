2ced328c85af8ae3ca91722c799f06bfbc2f2442
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-02-03 12:16:37 +0100

    main page done

--- angular-app/src/app/main-page/main-page.component.ts
+++ angular-app/src/app/main-page/main-page.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
 
@@ -12,5 +11,19 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  scrollToInfoSection() {
+    const infoSection = document.getElementById('info-section');
+    if (infoSection) {
+      infoSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToProjectsSection() {
+    const projectsSection = document.getElementById('projects-section');
+    if (projectsSection) {
+      projectsSection.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
