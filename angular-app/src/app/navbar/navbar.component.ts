b77159573089f7e51ab0804647551cd9560271b5
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-02-16 16:22:43 +0100

    small changes in navbar

--- angular-app/src/app/navbar/navbar.component.ts
+++ angular-app/src/app/navbar/navbar.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports: [CommonModule]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  dropdownVisible = false;
+
+  showDropdown() {
+    this.dropdownVisible = true;
+  }
+
+  hideDropdown() {
+    this.dropdownVisible = false;
+  }
+}
